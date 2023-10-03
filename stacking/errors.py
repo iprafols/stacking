37a7ea2bf51531965af67015fa81a4f60a7d5765
Ignasi Pérez-Ràfols <iprafols@gmail.com>
2023-10-03 20:55:58 +0200

    improved error logging

--- stacking/errors.py
+++ stacking/errors.py
@@ -1,51 +1,54 @@
 """This module define the different Error types related to the
 package stacking
 """
-
-
-class ConfigError(Exception):
-    """
-        Exceptions occurred in class Config
-    """
-
-
-class NormalizerError(Exception):
-    """
-        Exceptions occurred in class Normalizer or its childs
-    """
-
-
-class ReaderError(Exception):
-    """
-        Exceptions occurred in class Reader or its childs
-    """
-
-
-class RebinError(Exception):
-    """
-        Exceptions occurred in class Rebin
-    """
-
-
-class SpectrumError(Exception):
-    """
-        Exceptions occurred in class Spectrum
-    """
-
-
-class StackerError(Exception):
-    """
-        Exceptions occurred in class Stacker or its childs
-    """
-
+import logging
 
 class StackingError(Exception):
     """
         General exceptions ocurred in the package Stacking
     """
+    def __init__(self, message):
+        self.logger = logging.getLogger(__name__)
+        self.logger.error(message)
+        super().__init__(message)
+
+class ConfigError(StackingError):
+    """
+        Exceptions occurred in class Config
+    """
 
 
-class WriterError(Exception):
+class NormalizerError(StackingError):
+    """
+        Exceptions occurred in class Normalizer or its childs
+    """
+
+
+class ReaderError(StackingError):
+    """
+        Exceptions occurred in class Reader or its childs
+    """
+
+
+class RebinError(StackingError):
+    """
+        Exceptions occurred in class Rebin
+    """
+
+
+class SpectrumError(StackingError):
+    """
+        Exceptions occurred in class Spectrum
+    """
+
+
+class StackerError(StackingError):
+    """
+        Exceptions occurred in class Stacker or its childs
+    """
+
+
+class WriterError(StackingError):
     """
         Exceptions occurred in class Writer or its childs
     """
