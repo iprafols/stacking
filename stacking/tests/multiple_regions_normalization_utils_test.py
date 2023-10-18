efb85b4cb706b9137c76dba824a94641f0ff4863
Ignasi Pérez-Ràfols <iprafols@gmail.com>
2023-10-18 18:11:06 +0200

    added test

--- stacking/tests/multiple_regions_normalization_utils_test.py
+++ stacking/tests/multiple_regions_normalization_utils_test.py
@@ -81,6 +81,40 @@ class MultipleRegionsNormalizationUtilsTest(AbstractTest):
         self.assertTrue(np.allclose(results_jit, results_python,
                                     equal_nan=True))
 
+    def test_compute_norm_factors_no_sigma_i(self):
+        """Test function compute_norm_factors"""
+        # we run a single interval with positive flux and 0 sigma_I
+        flux = np.arange(10, dtype=float) - 5.0
+        ivar = np.arange(10, dtype=float) / 5.
+        wavelength = np.arange(10, dtype=float)
+        num_intervals = 1
+        intervals = np.array([(5., 10.)])
+        sigma_i2 = 0.0
+
+        results_jit = compute_norm_factors(flux,
+                                           ivar,
+                                           wavelength,
+                                           num_intervals,
+                                           intervals,
+                                           sigma_i2=sigma_i2)
+
+        self.assertTrue(results_jit.shape[0] == 4)
+        self.assertTrue(np.isclose(results_jit[0], 2.2857142857142856))
+        self.assertTrue(np.isclose(results_jit[1], 2.704493615131253))
+        self.assertTrue(results_jit[2] == 5)
+        self.assertTrue(np.isclose(results_jit[3], 7.0))
+
+        results_python = compute_norm_factors.py_func(flux,
+                                                      ivar,
+                                                      wavelength,
+                                                      num_intervals,
+                                                      intervals,
+                                                      sigma_i2=sigma_i2)
+
+        self.assertTrue(results_jit.shape[0] == results_python.shape[0])
+        self.assertTrue(np.allclose(results_jit, results_python,
+                                    equal_nan=True))
+
     def test_save_correction_factors_ascii(self):
         """Test function save_correction_factors_ascii"""
         out_file = f"{THIS_DIR}/results/save_correction_factors_ascii.txt"
